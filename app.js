ba90273922bf8aef248208f86604111ea9844bc4
Jeremy Lincoln <bossco28@gmail.com>
2017-11-16 08:40:30 -0500

    Pull geocode function out into itss own dir and file

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
-const request = require('request');
 const yargs = require('yargs');
+
+const geocode = require('./geocode/geocode')
 
 const argv = yargs
     .options({
@@ -14,27 +15,8 @@ const argv = yargs
     .alias('help', 'h')
     .argv;
 
-console.log(argv.a);
-var encodedAddress = encodeURIComponent(argv.a);
-request({
+geocode.geocodeAddress(argv.a)
 
-    url: `https://maps.googleapis.com/maps/api/geocode/json?address=${encodedAddress}&key=AIzaSyCr1ZuxSwJSUiJV37KacSg3uJ4eLv38tXY`,
-    json: true //basically saying, hey if you can send back JSON that would be cool
-}, (error, response, body)=> {
-    //pretty print the json object this is the org  console.log(body.results[0].geometry);
-    //console.log(JSON.stringify(body.results[0].geometry, undefined, 2));
-    //console.log(response.body);
-    //console.log(error);
-    if(error) {
-        console.log('Unable to connect to Google Servers.');
-    } else if(body.status === "ZERO_RESULTS") {  
-        console.log('Unable to find that address');
-    } else if(body.status === "OK") {
-        console.log(`Address: ${body.results[0].formatted_address}`);
-        console.log(`Latitude: ${body.results[0].geometry.location.lat}`);
-        console.log(`Longitude: ${body.results[0].geometry.location.lng}`);
-    }
-    
-    
-});
+
+
 
---
+++ geocode/geocode.js
@@ -0,0 +1,29 @@
+const request = require('request');
+
+var geocodeAddress = (addresss) => { 
+    var encodedAddress = encodeURIComponent(addresss);
+    request({
+        url: `https://maps.googleapis.com/maps/api/geocode/json?address=${encodedAddress}&key=AIzaSyCr1ZuxSwJSUiJV37KacSg3uJ4eLv38tXY`,
+        json: true //basically saying, hey if you can send back JSON that would be cool
+    }, (error, response, body)=> {
+        //pretty print the json object this is the org  console.log(body.results[0].geometry);
+        //console.log(JSON.stringify(body.results[0].geometry, undefined, 2));
+        //console.log(response.body);
+        //console.log(error);
+        if(error) {
+            console.log('Unable to connect to Google Servers.');
+        } else if(body.status === "ZERO_RESULTS") {  
+            console.log('Unable to find that address');
+        } else if(body.status === "OK") {
+            console.log(`Address: ${body.results[0].formatted_address}`);
+            console.log(`Latitude: ${body.results[0].geometry.location.lat}`);
+            console.log(`Longitude: ${body.results[0].geometry.location.lng}`);
+        }
+        
+        
+    });
+}
+
+module.exports = {
+    geocodeAddress
+}
