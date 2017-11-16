bbb8d65fc8fd54b59caad6cab4e05c32cda4ccb2
Jeremy Lincoln <bossco28@gmail.com>
2017-11-16 08:42:44 -0500

    Clean up formatting

--- geocode/geocode.js
+++ geocode/geocode.js
@@ -18,9 +18,7 @@ var geocodeAddress = (addresss) => {
             console.log(`Address: ${body.results[0].formatted_address}`);
             console.log(`Latitude: ${body.results[0].geometry.location.lat}`);
             console.log(`Longitude: ${body.results[0].geometry.location.lng}`);
-        }
-        
-        
+        }  
     });
 }
 
