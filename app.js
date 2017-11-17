768c591b90da806733947b616e5c2dddde234d92
Jeremy Lincoln <bossco28@gmail.com>
2017-11-17 08:34:47 -0500

    Add in weather dir weather.js

--- app.js
+++ app.js
@@ -1,20 +1,21 @@
-// const yargs = require('yargs');
-// const geocode = require('./geocode/geocode')
+const yargs = require('yargs');
+const geocode = require('./geocode/geocode')
+const weather = require('./weather/weather')
 
-// const argv = yargs
-//     .options({
-//         a: {
-//             demand: true,
-//             alias: 'address',
-//             describe: 'Address to fetch weather for',
-//             string: true
-//         }
-//     })
-//     .help('Format address by wrapping it with quotes')
-//     .alias('help', 'h')
-//     .argv;
+const argv = yargs
+    .options({
+        a: {
+            demand: true,
+            alias: 'address',
+            describe: 'Address to fetch weather for',
+            string: true
+        }
+    })
+    .help('Format address by wrapping it with quotes')
+    .alias('help', 'h')
+    .argv;
 
-//                                 //our callback will have either an errorMsg or a result
+                                //our callback will have either an errorMsg or a result
 // geocode.geocodeAddress(argv.a, (errorMessage, results) => {
 //     if(errorMessage) {
 //         console.log(errorMessage);
@@ -23,20 +24,16 @@
 //     }
 // });
 
-// b507ec2b86a8e1d8d9ff094fe6feb855
-const request = require('request');
-
-request({
-    url: `https://api.darksky.net/forecast/b507ec2b86a8e1d8d9ff094fe6feb855/36.170584,-82.3912`,
-    json: true //basically saying, hey if you can send back JSON that would be cool
-}, (error, response, body)=> {
-    if(!error && response.statusCode === 200){
-        console.log(body.currently.temperature);
-    } else {
-        console.log('Unable to fetch weather');
-    }
+//lat, lng, callback
+weather.getWeather(36.170584, -82.3912, (errorMessage, weatherResults) => {
     
+    if(errorMessage) {
+        console.log(errorMessage)
+    } else {
+        console.log(JSON.stringify(weatherResults, undefined, 2));
+    }
 });
+
 
 
 
---
+++ weather/weather.js
@@ -0,0 +1,23 @@
+const request = require('request');
+const env = require('dotenv').config();
+
+const api = process.env.API_KEY;
+
+var getWeather = (lat, lng, callback) => {
+    request({
+        url: `https://api.darksky.net/forecast/${api}/${lat},${lng}`,
+        json: true //basically saying, hey if you can send back JSON that would be cool
+    }, (error, response, body)=> {
+        if(!error && response.statusCode === 200){
+            console.log(body.currently.temperature);
+            callback(undefined, `It's currently ${body.currently.temperature} outside, but feels like ${body.currently.apparentTemperature}`);
+        } else {
+            callback('Unable to fetch weather');
+        }
+        
+    });
+}
+
+
+
+module.exports.getWeather = getWeather;
