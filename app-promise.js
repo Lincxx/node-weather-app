21979bf586ac4f55b0f711d6670dfd5a20805c86
Jeremy Lincoln <bossco28@gmail.com>
2017-11-20 11:55:51 -0500

    Add in some default and high & low daily temp

--- app-promise.js
+++ app-promise.js
@@ -17,6 +17,10 @@ const argv = yargs
     .alias('help', 'h')
     .argv;
 
+if(argv.address === "") {
+    argv.address = "48503"
+} 
+
 var encodedAddress = encodeURIComponent(argv.address);
 var geoCodeUrl = `https://maps.googleapis.com/maps/api/geocode/json?address=${encodedAddress}&key=AIzaSyCr1ZuxSwJSUiJV37KacSg3uJ4eLv38tXY`;
 
@@ -27,13 +31,18 @@ axios.get(geoCodeUrl).then((response) => {
     var lat = response.data.results[0].geometry.location.lat;
     var lng = response.data.results[0].geometry.location.lng;
     var weatherUrl = `https://api.darksky.net/forecast/${api}/${lat},${lng}`;
+    console.log(weatherUrl);
 
     console.log(response.data.results[0].formatted_address);
     return axios.get(weatherUrl);
 }).then((response) => {
     var temperature = response.data.currently.temperature;
     var apparentTemperature = response.data.currently.apparentTemperature;
+    var temperatureHigh = response.data.daily.data[0].temperatureHigh;
+    var temperatureLow = response.data.daily.data[0].temperatureLow;
     console.log(`It's currently ${temperature}. It feels like ${apparentTemperature}`);
+    console.log(`Today's high is ${temperatureHigh} `);
+    console.log(`Today's high is ${temperatureLow} `);
 }).catch((e) => {
    if(e.code === 'ENOTFOUND'){
        console.log('Unable to connect to API servers');
@@ -41,3 +50,5 @@ axios.get(geoCodeUrl).then((response) => {
        console.log(e.message);
    }
 });
+
+//hi lo temp
